27b321db521cca57a9ff8e31bd5a732f4835067b
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-14 16:39:47 +1100

    add commands

---
+++ Commands/RandomCommands.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
+
+namespace RustyKnife
+{
+    public class RandomCommands : BaseCommandModule
+    {
+
+        [Command("add")]
+        [Description("Adds two numbers")]
+        public async Task Add(CommandContext ctx, [Description("First Number")] int num1, [Description("Second Number")] int num2)
+        {
+            await ctx.Channel
+                .SendMessageAsync((num1 + num2).ToString())
+                .ConfigureAwait(false);
+        }
+        [Command("quote")]
+        [Description("A random Shaco quote")]
+        public async Task Quote(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync(Shaco.RandomVoiceLine().Description).ConfigureAwait(false);
+        }
+        [Command("hello")]
+        [Description("Says hello")]
+        public async Task Hello(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello " + ctx.Member.Nickname + "...").ConfigureAwait(false);
+        }
+        [Command("age")]
+        [Description("Displays the age of the user's account")]
+        public async Task Age(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Account created: " + ctx.Message.Author.CreationTimestamp.DateTime).ConfigureAwait(false);
+        }
+        [Command("roll")]
+        [Description("Random dice roll, default max is 100")]
+        public async Task Roll(CommandContext ctx)
+        {
+            Random rand = new Random();
+            int result = rand.Next(1, 101);
+
+            await ctx.Message.RespondAsync($"{ctx.Member.Nickname} rolled a {result}");
+        }
+        [Command("roll")]
+        [Description("Random dice roll")]
+        public async Task Roll(CommandContext ctx, string maxRoll)
+        {
+            int max = 0;
+            int result = 0;
+
+            int.TryParse(maxRoll, out max);
+            Random rand = new Random();
+            if (max >= 1)
+            {
+                result = rand.Next(1, max + 1);
+                await ctx.Message.RespondAsync($"{ctx.Member.Nickname} rolled a {result}");
+            }
+            else
+            {
+                await ctx.Message.RespondAsync($"Invalid argument, enter a positive number");
+            }
+        }
+    }
+}
---
+++ Commands/VoiceCommands.cs
@@ -0,0 +1,191 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
+
+namespace RustyKnife
+{
+    /*
+    how to get voice working
+    need to have VoiceNext package installed from nuget
+    need to have opus and sodium library files in main directory if VoiceNext wasnt installed using nuget
+    need to have ffmpeg executables located on computer somewhere
+    Resources
+    https://dsharpplus.github.io/articles/audio/voicenext/prerequisites.html
+    https://dsharpplus.readthedocs.io/en/stable/articles/voicenext.html
+    https://github.com/DSharpPlus/Example-Bots/blob/master/DSPlus.Examples.CSharp.Ex04/ExampleVoiceCommands.cs
+    */
+    public class VoiceCommands : BaseCommandModule
+    {
+        [Command("join"), Description("Bot joins voice channel the message writer is currently in")]
+        public async Task JoinCommand(CommandContext ctx, DiscordChannel channel = null)
+        {
+            channel ??= ctx.Member.VoiceState?.Channel;
+            await channel.ConnectAsync();
+        }
+        [Command("leave"), Description("Bot leaves voice channel")]
+        public async Task LeaveCommand(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNext();
+            var connection = vnext.GetConnection(ctx.Guild);
+
+            connection.Disconnect();
+            await ctx.RespondAsync("Bye bye...");
+        }
+        [Command("playquote"), Description("Plays a Shaco quote in a voice channel. Use ?join command for bot to join channel")]
+        public async Task PlayQuote(CommandContext ctx,
+        [Description("Enter a number between 1 and 10 to play a specific quote, enter anything else to play a random quote.\n1. How about a magic trick?\n2.The joke's on you!\n3. Look... behind you.\n4. This will be fun!\n5. Here we go!\n6. March, march, march, march!\n7. Now you see me, now you don't!\n8. Just a little bit closer!\n9. Why so serious?\n10. For my next trick, I'll make you disappear!")] string quote)
+        {
+            VoiceLine voiceLine;
+            int index = 0;
+            int.TryParse(quote, out index);
+            if (index > 0 && index <= 10)
+            {
+                voiceLine = Shaco.voiceLines[index - 1];
+            }
+            else
+            {
+                voiceLine = Shaco.RandomVoiceLine();
+            }
+
+            // check whether VNext is enabled
+            var vnext = ctx.Client.GetVoiceNext();
+            if (vnext == null)
+            {
+                // not enabled
+                await ctx.RespondAsync("VNext is not enabled or configured.");
+                return;
+            }
+
+            // check whether we aren't already connected
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+            {
+                // already connected
+                await ctx.RespondAsync("Not connected to voice channel, type ?join");
+                return;
+            }
+
+            // check if file exists
+            if (!File.Exists(voiceLine.Path))
+            {
+                // file does not exist
+                await ctx.RespondAsync($"File `{voiceLine.Path}` does not exist.");
+                return;
+            }
+
+            // wait for current playback to finish
+            while (vnc.IsPlaying)
+            {
+                await vnc.WaitForPlaybackFinishAsync();
+            }
+
+            // play
+            Exception exc = null;
+            await ctx.Message.RespondAsync($"{voiceLine.Description}");
+
+            try
+            {
+                await vnc.SendSpeakingAsync(true);
+
+                var psi = new ProcessStartInfo
+                {
+                    FileName = $@"ffmpeg\ffmpeg.exe",
+                    Arguments = $@"-i ""{voiceLine.Path}"" -ac 2 -f s16le -ar 48000 pipe:1 -loglevel quiet",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+                var ffmpeg = Process.Start(psi);
+                var ffout = ffmpeg.StandardOutput.BaseStream;
+
+                var txStream = vnc.GetTransmitSink();
+                await ffout.CopyToAsync(txStream);
+                await txStream.FlushAsync();
+                await vnc.WaitForPlaybackFinishAsync();
+            }
+            catch (Exception ex) { exc = ex; }
+            finally
+            {
+                await vnc.SendSpeakingAsync(false);
+                // await ctx.Message.RespondAsync($"Finished playing `{file}`");
+            }
+
+            if (exc != null)
+                await ctx.RespondAsync($"An exception occured during playback: `{exc.GetType()}: {exc.Message}`");
+        }
+        [Command("laugh"), Description("Shaco laughs in a voice channel. Use ?join command for bot to join channel")]
+        public async Task Laugh(CommandContext ctx)
+        {
+            VoiceLine voiceLine = Shaco.RandomLaugh();
+
+            // check whether VNext is enabled
+            var vnext = ctx.Client.GetVoiceNext();
+            if (vnext == null)
+            {
+                // not enabled
+                await ctx.RespondAsync("VNext is not enabled or configured.");
+                return;
+            }
+
+            // check whether we aren't already connected
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+            {
+                // already connected
+                await ctx.RespondAsync("Not connected to voice channel, type ?join");
+                return;
+            }
+
+            // check if file exists
+            if (!File.Exists(voiceLine.Path))
+            {
+                // file does not exist
+                await ctx.RespondAsync($"File `{voiceLine.Path}` does not exist.");
+                return;
+            }
+
+            // wait for current playback to finish
+            while (vnc.IsPlaying)
+            {
+                await vnc.WaitForPlaybackFinishAsync();
+            }
+
+            // play
+            Exception exc = null;
+            await ctx.Message.RespondAsync($"{voiceLine.Description}");
+
+            try
+            {
+                await vnc.SendSpeakingAsync(true);
+
+                var psi = new ProcessStartInfo
+                {
+                    FileName = $@"ffmpeg\ffmpeg.exe",
+                    Arguments = $@"-i ""{voiceLine.Path}"" -ac 2 -f s16le -ar 48000 pipe:1 -loglevel quiet",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+                var ffmpeg = Process.Start(psi);
+                var ffout = ffmpeg.StandardOutput.BaseStream;
+
+                var txStream = vnc.GetTransmitSink();
+                await ffout.CopyToAsync(txStream);
+                await txStream.FlushAsync();
+                await vnc.WaitForPlaybackFinishAsync();
+            }
+            catch (Exception ex) { exc = ex; }
+            finally
+            {
+                await vnc.SendSpeakingAsync(false);
+                // await ctx.Message.RespondAsync($"Finished playing `{file}`");
+            }
+
+            if (exc != null)
+                await ctx.RespondAsync($"An exception occured during playback: `{exc.GetType()}: {exc.Message}`");
+        }
+    }
+}
