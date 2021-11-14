ecee210f6c9a225254798f306c3998dda4672539
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-14 14:19:35 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShacoDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
