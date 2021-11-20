00bc074682a29d8a3bfbc09eaa28effcecbd61b5
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-20 17:21:56 +1100

    create poll commands class

---
+++ Commands/PollCommands.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace ShacoDiscordBot
+{
+    public class PollCommands : VoiceNextCommands
+    {
+
+    }
+}
