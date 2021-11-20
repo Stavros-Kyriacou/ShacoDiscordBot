297ce2ecadb6b488a0dd19081b3759b1595dbdcc
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-20 17:19:12 +1100

    fix namespace, instantiate GameController, subscrive GC message handler to Client.MessageCreated, register GameCommands

--- Models/Bot.cs
+++ Models/Bot.cs
@@ -6,25 +6,29 @@ using DSharpPlus.VoiceNext;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 
-namespace RustyKnife
+namespace ShacoDiscordBot
 {
     public class Bot
     {
         public DiscordClient Client { get; private set; }
         public CommandsNextExtension Commands { get; private set; }
-        public VoiceNextExtension Voice {get; private set; }
+        public VoiceNextExtension Voice { get; private set; }
+        public GameController GameController {get; private set;}
+        // public List<User> users = new List<User>();
         public async Task RunAsync()
         {
             var json = "";
 
             using (var fileStream = File.OpenRead("config.json"))
             {
-                using(var streamReader = new StreamReader(fileStream, new UTF8Encoding(false)))
+                using (var streamReader = new StreamReader(fileStream, new UTF8Encoding(false)))
                 {
                     json = await streamReader.ReadToEndAsync().ConfigureAwait(false);
                 }
@@ -39,14 +43,16 @@ namespace RustyKnife
                 AutoReconnect = true,
                 MinimumLogLevel = LogLevel.Debug
             };
+            this.GameController = new GameController();
 
             this.Client = new DiscordClient(config);
 
+            this.Client.MessageCreated += GameController.MessageHandler;
             this.Client.Ready += OnClientReady;
 
             var commandsConfig = new CommandsNextConfiguration
             {
-                StringPrefixes = new string[] {configJson.Prefix},
+                StringPrefixes = new string[] { configJson.Prefix },
                 EnableDms = false,
                 EnableMentionPrefix = true,
                 DmHelp = false
@@ -56,8 +62,13 @@ namespace RustyKnife
 
             this.Commands.RegisterCommands<RandomCommands>();
             this.Commands.RegisterCommands<VoiceCommands>();
+            this.Commands.RegisterCommands<GameCommands>();
+
+            // GameCommands commands = new GameCommands(this.GameController);
 
             this.Voice = Client.UseVoiceNext();
+
+            await this.GameController.Load();
 
             await this.Client.ConnectAsync();
 
