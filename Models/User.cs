863b36801cc18e545d2abf664367da37d96d58b5
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-22 17:58:29 +1100

    add GoldGifted and GoldReceived properties. add class constructor

--- Models/User.cs
+++ Models/User.cs
@@ -9,5 +9,17 @@ namespace ShacoDiscordBot
         public int Gold { get; set; }
         public int TimesCollected { get; set; }
         public DateTime LastGoldCollectionTime { get; set; }
+        public int GoldGifted { get; set; }
+        public int GoldReceived { get; set; }
+        public User(ulong Id, string username)
+        {
+            this.ID = Id;
+            this.UserName = username;
+            this.Gold = 0;
+            this.TimesCollected = 0;
+            this.LastGoldCollectionTime = DateTime.Now;
+            this.GoldGifted = 0;
+            this.GoldReceived = 0;
+        }
     }
 }
