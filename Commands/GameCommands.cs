0718b555b7993d5b09ab9405c5b67a69293d4497
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-12-20 15:50:56 +1100

    add profile, shop, buy and leaderboard commands

--- Commands/GameCommands.cs
+++ Commands/GameCommands.cs
@@ -4,6 +4,7 @@ using DSharpPlus.CommandsNext.Attributes;
 using System.Collections.Generic;
 using DSharpPlus.Entities;
 using System;
+using System.Linq;
 
 namespace ShacoDiscordBot
 {
@@ -79,13 +80,12 @@ namespace ShacoDiscordBot
         [Command("test")]
         public async Task Test(CommandContext ctx)
         {
-
-            var embed = new DiscordEmbedBuilder
-            {
-                Title = "Test Embed",
-                Description = ":)"
-            };
-            await ctx.RespondAsync(embed: embed);
+            var span = new TimeSpan(0, 0, 61);
+            var minutes = span.Duration().Minutes;
+            var seconds = span.Duration().Seconds;
+            string time = $"{minutes}m {seconds}s";
+            await ctx.Channel.SendMessageAsync(time);
+            await ctx.Channel.SendMessageAsync("end");
         }
         public DiscordEmbedBuilder UserProfile(User user)
         {
@@ -94,14 +94,194 @@ namespace ShacoDiscordBot
                 Title = $"{user.UserName}'s Profile",
                 Color = DiscordColor.Red
             };
+
+            var span = user.LastCollectionTime.AddSeconds(user.CollectionCooldown) - user.LastCollectionTime;
+            var cooldown = $"{span.Duration().Minutes}m {span.Duration().Seconds}s";
+
             embed.AddField("Gold", user.Gold.ToString())
-                    .AddField("Times Collected", user.TimesCollected.ToString())
-                    .AddField("Gold Gifted", user.GoldGifted.ToString())
-                    .AddField("Gold Received", user.GoldReceived.ToString())
-                    .AddField("Last Collection Time", user.LastGoldCollectionTime.ToString())
-                    .AddField("Next Collection TIme", user.LastGoldCollectionTime.AddSeconds(GameManager.CollectionCooldown).ToString());
+                    .AddField("Gold Spent", user.GoldSpent.ToString(), true)
+                    .AddField("Gold Gifted", user.GoldGifted.ToString(), true)
+                    .AddField("Gold Received", user.GoldReceived.ToString(), true)
+                    .AddField("\u200b", "\u200b") //empty space
+
+                    .AddField("Next Collection TIme", user.LastCollectionTime.AddSeconds(user.CollectionCooldown).ToString(), true)
+                    .AddField("Last Collection Time", user.LastCollectionTime.ToString(), true)
+                    .AddField("Times Collected", user.TimesCollected.ToString(), true)
+                    .AddField("\u200b", "\u200b") //empty space
+
+                    .AddField("Collection Amount", user.CollectionAmount.ToString(), true)
+                    .AddField("Level", user.CollectionLevel.ToString(), true)
+                    .AddField("Upgrade Cost", user.CollectionUpgradeCost.ToString(), true)
+                    .AddField("\u200b", "\u200b") //empty space
+
+                    .AddField("Collection Cooldown", cooldown, true)
+                    .AddField("Level", user.CooldownLevel.ToString(), true)
+                    .AddField("Upgrade Cost", user.CooldownUpgradeCost.ToString(), true)
+                    ;
             embed.WithFooter(Shaco.RandomVoiceLine().Description);
-            return embed; 
+            return embed;
+        }
+        public DiscordEmbedBuilder UserShop(User user)
+        {
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = $"{user.UserName}'s Shop",
+                Color = DiscordColor.Yellow
+            };
+
+            var span = user.LastCollectionTime.AddSeconds(user.CollectionCooldown) - user.LastCollectionTime;
+            var cooldown = $"{span.Duration().Minutes}m {span.Duration().Seconds}s";
+
+            embed.AddField("1. Collection Amount Upgrade", "\u200b") //empty space
+                    .AddField("Cost", user.CollectionUpgradeCost.ToString(), true)
+                    .AddField("Current Level", user.CollectionLevel.ToString(), true)
+                    .AddField("Current Collection Amount", user.CollectionAmount.ToString(), true)
+                    .AddField("\u200b", "\u200b")
+
+                    .AddField("2. Collection Cooldown Upgrade", "\u200b")
+                    .AddField("Cost", user.CooldownUpgradeCost.ToString(), true)
+                    .AddField("Current Level", user.CooldownLevel.ToString(), true)
+                    .AddField("Current Collection Cooldown", cooldown, true)
+                    ;
+            embed.WithFooter(Shaco.RandomVoiceLine().Description);
+            return embed;
+        }
+        [Command("shop")]
+        [Description("View upgrades available for purchase. Use ?buy {shopNumber} to purchase upgrades")]
+        public async Task Shop(CommandContext ctx)
+        {
+            var user = GameManager.GetUserById(ctx.Message.Author.Id);
+            var embed = UserShop(user);
+            await ctx.RespondAsync(embed: embed);
+        }
+        [Command("buy")]
+        [Description("Purchase gold collection and cooldown upgrades. Use ?shop to view upgrades available for purchase")]
+        public async Task Buy(CommandContext ctx, [Description("The number of the shop item. Use ?shop to view upgrades available for purchase")] int shopNumber)
+        {
+            var user = GameManager.GetUserById(ctx.Message.Author.Id);
+
+            switch (shopNumber)
+            {
+                case 1:
+                    if (user.Gold >= user.CollectionUpgradeCost)
+                    {
+                        user.Gold -= user.CollectionUpgradeCost;
+                        user.GoldSpent += user.CollectionUpgradeCost;
+                        user.CollectionLevel++;
+
+                        await ctx.RespondAsync("Gold Collection Upgrade Purchased!");
+                        await GameManager.Save();
+                    }
+                    else
+                    {
+                        await ctx.RespondAsync("Not enough funds :(");
+                    }
+                    break;
+                case 2:
+                    if (user.CooldownLevel >= user.CooldownMaxLevel)
+                    {
+                        await ctx.RespondAsync("Cooldown Upgrade at Max Level!\nNo Furthar Upgrades Available");
+                        break;
+                    }
+                    if (user.Gold >= user.CooldownUpgradeCost)
+                    {
+                        user.Gold -= user.CooldownUpgradeCost;
+                        user.GoldSpent += user.CooldownUpgradeCost;
+                        user.CooldownLevel++;
+
+                        await ctx.RespondAsync("Gold Collection Cooldown Upgrade Purchased!");
+                        await GameManager.Save();
+                    }
+                    else
+                    {
+                        await ctx.RespondAsync("Not enough funds :(");
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        [Command("leaderboard")]
+        public async Task Leaderboard(CommandContext ctx)
+        {
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Current Gold Leaderboard",
+                Color = DiscordColor.Green
+            };
+            var leaders = GameManager.Users.OrderByDescending(u => u.Gold).ToList();
+
+            for (int i = 0; i < leaders.Count; i++)
+            {
+                embed.AddField($"{i + 1}. {leaders[i].UserName}", leaders[i].Gold.ToString());
+            }
+            await ctx.RespondAsync(embed: embed);
+        }
+        [Command("leaderboard")]
+        public async Task Leaderboard(CommandContext ctx, string type)
+        {
+            List<User> leaders = new List<User>();
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Green
+            };
+
+            switch (type)
+            {
+                case "total":
+                    List<UserGoldTotal> goldTotals = new List<UserGoldTotal>();
+                    foreach (var u in GameManager.Users)
+                    {
+                        goldTotals.Add(new UserGoldTotal
+                        {
+                            username = u.UserName,
+                            goldGenerated = u.Gold + u.GoldSpent + u.GoldGifted
+                        });
+                    }
+                    var goldTotalsLeaderboard = goldTotals.OrderByDescending(u => u.goldGenerated).ToList();
+                    embed.Title = "Total Gold Generated Leaderboard";
+                    for (int i = 0; i < goldTotalsLeaderboard.Count; i++)
+                    {
+                        embed.AddField($"{i + 1}. {goldTotalsLeaderboard[i].username}", goldTotalsLeaderboard[i].goldGenerated.ToString());
+                    }
+                    break;
+                case "spent":
+                    leaders = GameManager.Users.OrderByDescending(u => u.GoldSpent).ToList();
+                    embed.Title = "Gold Spent Leaderboard";
+                    for (int i = 0; i < leaders.Count; i++)
+                    {
+                        embed.AddField($"{i + 1}. {leaders[i].UserName}", leaders[i].GoldSpent.ToString());
+                    }
+                    break;
+                case "gifted":
+                    leaders = GameManager.Users.OrderByDescending(u => u.GoldGifted).ToList();
+                    embed.Title = "Gold Gifted Leaderboard";
+                    for (int i = 0; i < leaders.Count; i++)
+                    {
+                        embed.AddField($"{i + 1}. {leaders[i].UserName}", leaders[i].GoldGifted.ToString());
+                    }
+                    break;
+                case "received":
+                    leaders = GameManager.Users.OrderByDescending(u => u.GoldReceived).ToList();
+                    embed.Title = "Gold Received Leaderboard";
+                    for (int i = 0; i < leaders.Count; i++)
+                    {
+                        embed.AddField($"{i + 1}. {leaders[i].UserName}", leaders[i].GoldReceived.ToString());
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+
+            await ctx.RespondAsync(embed: embed);
+        }
+        struct UserGoldTotal
+        {
+            public string username;
+            public int goldGenerated;
         }
     }
 }
