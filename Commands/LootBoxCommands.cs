92093a7bf11dab8a9bb028a1f5e6f891a7fc0177
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2022-01-27 23:06:09 +1100

    add openbox method

--- Commands/LootBoxCommands.cs
+++ Commands/LootBoxCommands.cs
@@ -15,7 +15,60 @@ namespace ShacoDiscordBot
         public async Task Inventory(CommandContext ctx)
         {
             var user = GameManager.GetUserById(ctx.Message.Author.Id);
-            await ctx.RespondAsync("yo");
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = $"{user.UserName}'s Lootbox Inventory",
+                Color = DiscordColor.Magenta
+            };
+
+            for (int i = 0; i < user.LootBoxInventory.Length; i++)
+            {
+                embed.AddField($"{i + 1}. Cost: {GameManager.LootBoxes[i].Cost} Gold", user.LootBoxInventory[i].ToString());
+            }
+
+            await ctx.RespondAsync(embed: embed);
+        }
+        [Command("openbox")]
+        public async Task OpenBox(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            var user = GameManager.GetUserById(ctx.Message.Author.Id);
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Select Box to Open",
+                Color = DiscordColor.Magenta
+            };
+            embed.WithFooter("Enter a number between 1-6 to open that type of box");
+
+            for (int i = 0; i < user.LootBoxInventory.Length; i++)
+            {
+                embed.AddField($"{i + 1}. Lootbox value: {GameManager.LootBoxes[i].Cost} gold", $"Owned: {user.LootBoxInventory[i].ToString()}");
+            }
+
+            await ctx.RespondAsync(embed: embed);
+
+            var message = await interactivity.WaitForMessageAsync(x => x.Channel == ctx.Channel && x.Author == ctx.Message.Author);
+
+            if (int.TryParse(message.Result.Content, out int index))
+            {
+                if (index > 0 && index <= 6)
+                {
+                    if (user.LootBoxInventory[index - 1] > 0)
+                    {
+                        await ctx.RespondAsync(embed: LootBoxManager.OpenLootBox(index, user));
+                        await GameManager.Save();
+                    }
+                    else
+                    {
+                        await ctx.RespondAsync("You dont own any of that type of lootbox, either open a different type or buy some more, bitchass");
+                    }
+                }
+                else
+                {
+                    await ctx.RespondAsync("Invalid argument");
+                }
+            }
         }
     }
 }
